fa8e7061974c8a129870ba2fde5c970bc930e0da
mugeshr19 <mugesh.r1906@gmail.com>
2025-12-15 10:44:25 +0530

    created an structure for the homepage

---
+++ Movie-ticket-booking-frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import AppRoutes from './routes/AppRoutes';
+
+function App() {
+  return (
+    <Router>
+      <div className="min-h-screen bg-gray-900 text-white">
+        <Navbar />
+        <AppRoutes />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
